e92688231850b3376792f9e17d65e8856ccc1abe
bomberatox <bomberatox@gmail.com>
2021-06-23 22:01:10 -0300

    Adição controllers, core, template, view

---
+++ Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <?php
+    /* IMPORTAÇÕES */
+    require_once 'app/Core/Core.php';
+    require_once 'app/Controller/HomeController.php';
+    require_once 'app/Controller/ErroController.php';
+
+    $template = file_get_contents('App/Template/Estrutura.html');
+
+    ob_start();
+    $core = new Core;
+    $core->start($_GET);
+
+    $saida = ob_get_contents();
+    ob_end_clean();
+
+    $tplPronto = str_replace('{{cadastros}}', $saida, $template);
+    echo $tplPronto;
+    ?>
+</body>
+</html>
